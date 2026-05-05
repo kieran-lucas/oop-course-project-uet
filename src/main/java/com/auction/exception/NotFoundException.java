d57f298b0fb3200ae44629bc94cd2028d0edeb67
HumaNormal <buingocphuhung@gmail.com>
2026-05-06 01:34:02 +0700

    feat(NotFoundException): add constructor for exception chaining

--- src/main/java/com/auction/exception/NotFoundException.java
+++ src/main/java/com/auction/exception/NotFoundException.java
@@ -6,4 +6,8 @@ public class NotFoundException extends AuctionException {
   public NotFoundException(String message) {
     super(message);
   }
+
+  public NotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
