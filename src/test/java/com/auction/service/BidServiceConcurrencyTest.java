8e9fe570e2c57d30b2f0e66911f4fe0ee99c16a9
Kieran Lucas <kieranlucas.work@gmail.com>
2026-05-24 11:29:13 +0700

    fix: add missing DisplayName import to BidServiceConcurrencyTest

--- src/test/java/com/auction/service/BidServiceConcurrencyTest.java
+++ src/test/java/com/auction/service/BidServiceConcurrencyTest.java
@@ -27,6 +27,7 @@ import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
